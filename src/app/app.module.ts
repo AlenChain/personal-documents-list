ca91dbd07e45d9be08c65c4718353004dea7a23a
Nikita Mikhailov <alenchain@yandex.ru>
2022-08-27 15:33:42 +0300

    initialize components and angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddDocumentModalComponent } from './add-document-modal/add-document-modal.component';
+import { SearchPanelComponent } from './search-panel/search-panel.component';
+import { WorkWithDocumentsPanelComponent } from './work-with-documents-panel/work-with-documents-panel.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { DocumentsTableComponent } from './documents-table/documents-table.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddDocumentModalComponent,
+    SearchPanelComponent,
+    WorkWithDocumentsPanelComponent,
+    MainPageComponent,
+    DocumentsTableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/work-with-documents-panel/work-with-documents-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-work-with-documents-panel',
+  templateUrl: './work-with-documents-panel.component.html',
+  styleUrls: ['./work-with-documents-panel.component.scss']
+})
+export class WorkWithDocumentsPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
